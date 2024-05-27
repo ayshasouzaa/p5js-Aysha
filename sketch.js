df9b73c911e43539acfcff9c24aaabffc4522bf7
ayshasouzaa <00001113813039SP@aluno.educacao.sp.gov.br>
2024-05-27 20:51:02 -0300

    sketch.js

--- sketch.js
+++ sketch.js
@@ -1,41 +1,38 @@
-let cor;
-let posicaoHorizontal; // x
-let posicaoVertical; // y
+
+
+//sons do jogo
+let raquetada;
+let ponto;
+let trilha;
+
+let colidiu = false;
 
 function setup() {
-  createCanvas(400, 400);
-   background(color(100,0,0));
-  cor = color(random(0,255), random(0,255), random(0,255));
-  posicaoHorizontal = 200;
-  posicaoVertical = 200;
+  createCanvas(600, 400);
+    trilha.loop();
 }
-
 
 function draw() {
-  
-  fill(cor);
-  circle(posicaoHorizontal,posicaoVertical,50);
- 
-  
-  
-  if (mouseX < posicaoHorizontal){
-    posicaoHorizontal =  posicaoHorizontal - 1;
-  }
-  
-  if (mouseX > posicaoHorizontal){
-    posicaoHorizontal =  posicaoHorizontal + 1;
-  }
-  
-  if (mouseY < posicaoVertical){
-    posicaoVertical--;
-  }
-    
-  if (mouseY > posicaoVertical){
-    posicaoVertical++;
-  }
-  
-  if (mouseIsPressed){
-    cor = color(random(0,255), random(0,255), random(0,255), random(0,100));
-  }
-    
+    background(0);
+    mostraBolinha();
+    movimentaBolinha();
+    verificaColisaoBorda();
+    mostraRaquete(xRaquete, yRaquete);
+    movimentaMinhaRaquete();
+    verificaColisaoRaquete(xRaquete, yRaquete);
+    verificaColisaoRaquete(xRaqueteOponente, yRaqueteOponente);
+    mostraRaquete(xRaqueteOponente, yRaqueteOponente);
+    movimentaRaqueteOponente();
+    incluiPlacar() 
+    marcaPonto();
 }
+function mostraBolinha() {
+  circle(xBolinha, yBolinha, diametro);
+}
+
+function movimentaBolinha() {
+  xBolinha += velocidadeXBolinha;
+  yBolinha += velocidadeYBolinha;
+}
+
+function v
